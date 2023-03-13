ee4756078ca5438c9a5215fca79c9adc3382d746
Yaroslav Abrosimov <abrosimovyar@gmail.com>
2023-03-13 11:48:31 +0300

    Add authorization

---
+++ app/src/main/java/com/example/project418/screens/login/LoginFragment.kt
@@ -0,0 +1,39 @@
+package com.example.project418.screens.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.project418.databinding.FragmentLoginBinding
+import com.example.project418.models.Department
+import com.github.terrakok.cicerone.Router
+
+class LoginFragment(private val router: Router) : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    private val viewModel: LoginVM by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        viewModel.setRouter(router)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogin.setOnClickListener {
+            val userLogin = binding.etLogin.text.toString()
+            val userPassword = binding.etPassword.text.toString()
+
+            viewModel.auth(Department(login = userLogin, password = userPassword), requireContext())
+        }
+    }
+}
--- app/src/main/java/com/example/project418/storage/DataBaseHelper.kt
+++ app/src/main/java/com/example/project418/storage/DataBaseHelper.kt
@@ -1,8 +1,10 @@
 package com.example.project418.storage
 
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.example.project418.models.Department
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -37,7 +39,7 @@ class DataBaseHelper(private val context: Context) :
                 e.printStackTrace()
             }
         }
-        return SQLiteDatabase.openDatabase(dbFile.path, null, SQLiteDatabase.OPEN_READONLY)
+        return SQLiteDatabase.openDatabase(dbFile.path, null, SQLiteDatabase.OPEN_READWRITE)
     }
 
     private fun copyDatabase(dbFile: File) {
@@ -53,8 +55,32 @@ class DataBaseHelper(private val context: Context) :
 
     fun closeDb() = database.close()
 
+    fun getListOfDepartments(): List<Department> {
+        val returnList = mutableListOf<Department>()
+
+        val selectQuery = "select * from $DEPARTMENT_TABLE"
+        val cursor: Cursor = database.rawQuery(selectQuery, null)
+
+        if (cursor.moveToFirst())
+            do {
+                returnList.add(
+                    Department(
+                        cursor.getInt(0),
+                        cursor.getString(1),
+                        cursor.getString(2)
+                    )
+                )
+            } while (cursor.moveToNext())
+
+        cursor.close()
+
+        return returnList
+    }
+
     companion object {
         private const val DATABASE_NAME = "Project418"
         private const val DATABASE_VERSION = 1
+
+        private const val DEPARTMENT_TABLE = "Department"
     }
 }
